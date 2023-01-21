806c7355fcce4bba5d141dbbba123ce82e8c8196
Brent Simmons <brents@patriot1tech.com>
2023-01-21 15:22:14 -0400

    Adding initial idea

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # valentinesday
+A little fun for my wife on Valentine's Day.
---
+++ web/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Box-area">
+          <div className="Click-box" style={{ top: 0, left: 0 }}>
+          </div>
+          <div className="Click-box" style={{ top: "5%", left: "33%" }}>
+          </div>
+          <div className="Click-box shimmer" style={{ top: "10%", left: "10%" }}>
+          </div>
+          <div className="heart">
+
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
